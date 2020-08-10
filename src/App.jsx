23cd65118a92ffc079ec9d15d49fb4cafb5bf956
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-10 19:39:16 -0400

    Convert the App to use the new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
-import {
-  Container,
-} from 'semantic-ui-react';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Container } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 
-function App() {
-  return (
-    <Container>
-      <div>Menu</div>
-      <div>Page</div>
-    </Container>
-  );
+import './App.css';
+import handleInitialData from './actions/shared';
+import Navbar from './components/layout/Navbar';
+import Routes from './routes';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const {
+      props: {
+        dispatch,
+      },
+    } = this;
+
+    dispatch(handleInitialData());
+  }
+
+  render() {
+    const {
+      history,
+    } = this.props;
+
+    return (
+      <ConnectedRouter
+        history={history}
+      >
+        <Container>
+          <Navbar />
+          { Routes }
+        </Container>
+      </ConnectedRouter>
+    );
+  }
 }
 
-export default App;
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({}).isRequired,
+};
+
+export default connect()(App);
