a449f8b3d8dc6cb91e10e7a54521c0dfab935618
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-17 18:45:06 -0400

    Add support for new question in questions reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,6 +1,7 @@
 import {
   RECEIVE_QUESTIONS,
   ANSWER_QUESTION,
+  ADD_NEW_QUESTION,
 } from '../actions/questions';
 
 export default function questionsReducer(state = {}, action) {
@@ -26,6 +27,23 @@ export default function questionsReducer(state = {}, action) {
               ...state[info.qid][info.answer].votes,
               info.authedUser,
             ],
+          },
+        },
+      };
+    case ADD_NEW_QUESTION:
+      return {
+        ...state,
+        [info.id]: {
+          id: info.id,
+          author: info.author,
+          timestamp: info.timestamp,
+          optionOne: {
+            votes: info.optionOne.votes,
+            text: info.optionOne.text,
+          },
+          optionTwo: {
+            votes: info.optionTwo.votes,
+            text: info.optionTwo.text,
           },
         },
       };
