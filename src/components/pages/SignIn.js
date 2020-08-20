7970efc316fb4ef133d10833ce0fb47f9a5d4c7e
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2020-08-20 10:55:28 -0400

    Refactor SignIn from class to function
    * src/components/pages/SignIn.js: Use React hooks instead of the
    class system.

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import {
   Button,
@@ -19,114 +19,81 @@ import {
   redirectPropType,
 } from '../common';
 
-class SignIn extends Component {
-  constructor(...args) {
-    super(...args);
-    this.state = {
-      submitDisabled: true,
-      loading: false,
-    };
-  }
+function SignIn({
+  doSetAuthedUser,
+  authedUser,
+  doRedirectUndo,
+  redirect,
+}) {
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
 
-  componentDidUpdate() {
-    const {
-      authedUser,
-      doRedirectUndo,
-      redirect,
-    } = this.props;
+  const handleSubmit = () => {
+    doSetAuthedUser(user);
+    setLoading(true);
+  };
+  const handleUserSelected = (ev, data) => {
+    setUser(data.value);
+    setSubmitDisabled(data.value === '');
+  };
 
+  useEffect(() => {
     if (authedUser !== null) {
       doRedirectUndo(redirect);
     }
-  }
+  });
 
-  handleSubmit = () => {
-    const {
-      props: {
-        doSetAuthedUser,
-      },
-    } = this;
-
-    this.setState((oldState) => {
-      doSetAuthedUser(oldState.user);
-      // Add timeout
-
-      return {
-        loading: true,
-      };
-    });
-  }
-
-  handleUserSelected = (ev, data) => {
-    this.setState((oldState) => ({
-      ...oldState,
-      user: data.value,
-      submitDisabled: data.value === '',
-    }));
-  }
-
-  render() {
-    const {
-      submitDisabled,
-      loading,
-    } = this.state;
-
-    const {
-      handleUserSelected,
-      handleSubmit,
-    } = this;
-
-    return (
-      <Body>
-        <Label
-          attached="top"
-        >
-          <Header
-            as="h3"
-          >
-            Welcome to the Would You Rather App!
-            <Header.Subheader
-              color="teal"
-            >
-              Please sign in to continue
-            </Header.Subheader>
-          </Header>
-        </Label>
-        <Image
-          src={logo}
-          alt="Logo"
-          size="medium"
-          centered
-        />
+  return (
+    <Body>
+      <Label
+        attached="top"
+      >
         <Header
-          as="h2"
-          color="teal"
+          as="h3"
         >
-          Sign in
+          Welcome to the Would You Rather App!
+          <Header.Subheader
+            color="teal"
+          >
+            Please sign in to continue
+          </Header.Subheader>
         </Header>
-        <Form
-          onSubmit={handleSubmit}
-          loading={loading}
-        >
-          <Form.Field>
-            <UserSelector
-              onChange={handleUserSelected}
-            />
-          </Form.Field>
-          <Form.Field>
-            <Button
-              type="submit"
-              color="teal"
-              disabled={submitDisabled}
-              fluid
-            >
-              Submit
-            </Button>
-          </Form.Field>
-        </Form>
-      </Body>
-    );
-  }
+      </Label>
+      <Image
+        src={logo}
+        alt="Logo"
+        size="medium"
+        centered
+      />
+      <Header
+        as="h2"
+        color="teal"
+      >
+        Sign in
+      </Header>
+      <Form
+        onSubmit={handleSubmit}
+        loading={loading}
+      >
+        <Form.Field>
+          <UserSelector
+            onChange={handleUserSelected}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Button
+            type="submit"
+            color="teal"
+            disabled={submitDisabled}
+            fluid
+          >
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+    </Body>
+  );
 }
 
 SignIn.propTypes = {
